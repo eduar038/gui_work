aade51347f239f4785325ed3f8137eebed730d65
eduar038 <eduardogarcia777@gmail.com>
2025-07-01 23:17:55 -0500

    coronao

--- src/mayoredad.java
+++ src/mayoredad.java
@@ -22,9 +22,39 @@ public class mayoredad {
             } catch (NumberFormatException ex) {
                 JOptionPane.showMessageDialog(null,"Por favor, escribe un número válido.");
             }
+        }
+        );
+        verificarButton.addActionListener(e -> {
+            String texto = textField2.getText();
+
+            try {
+                int numero = Integer.parseInt(texto);
+
+                if (numero <= 0) {
+                    JOptionPane.showMessageDialog(null, "El número debe ser positivo.");
+                    return;
+                }
+
+                int suma = 0;
+                for (int i = 1; i < numero; i++) {
+                    if (numero % i == 0) {
+                        suma += i;
+                    }
+                }
+
+                if (suma == numero) {
+                    JOptionPane.showMessageDialog(null, numero + " es un número perfecto ");
+                } else {
+                    JOptionPane.showMessageDialog(null, numero + " no es un número perfecto ");
+                }
+
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Por favor ingresa un número válido.");
+            }
         });
+    }
 
     }
 
 
-}
+
